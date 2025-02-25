1de5e44cedd222999bcbbec07c4f0907f26b67a7
Marija <marija@dailyart.eu>
2025-02-25 09:19:26 +0200

    added zod, product , images, data from db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
-import sampleData from '@/app/db/sample-data';
-import ProductList from '@/components/shared/header/product/product-list';
-
+import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
+  // Convert rating to number
+  const formattedProducts = latestProducts.map(product => ({
+    ...product,
+    rating: Number(product.rating), // Convert rating to number
+  }));
+
   return ( 
     <div className='space-y-8'>
       <h2 className='h2-bold'>Latest Products</h2>
-      <ProductList title='Newest Arrivals' data={sampleData.products} limit={4} />
+      <ProductList title='Newest Arrivals' data={formattedProducts} limit={4} />
     </div>
   );
 };
