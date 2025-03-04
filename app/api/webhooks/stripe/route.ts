1be15abd16d70938b1375fd4ccb13f7a3cf22723
Marija <marija@dailyart.eu>
2025-03-04 16:34:41 +0200

    stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,35 +2,31 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 import { updateOrderToPaid } from '@/lib/actions/order.actions';
 
+// Initialize Stripe with the secret API key from environment variables
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
+// Define the POST handler function for the Stripe webhook
 export async function POST(req: NextRequest) {
-  const signature = req.headers.get('stripe-signature') as string;
-  let event: Stripe.Event;
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      await req.text(),
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    );
-  } catch (err) {
-    const error = err as Error;
-    return NextResponse.json({ error: error.message }, { status: 400 });
-  }
-
-  // Check for successful payment
+  // Construct the event using the raw request body, the Stripe signature header, and the webhook secret.
+  // This ensures that the request is indeed from Stripe and has not been tampered with.
+  const event = await stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get('stripe-signature') as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+  // charge.succeeded indicates a successful payment
   if (event.type === 'charge.succeeded') {
-    const charge = event.data.object as Stripe.Charge;
+    // Retrieve the order ID from the payment metadata
+    const { object } = event.data;
 
-    // Update order status
+    // Update the order status to paid
     await updateOrderToPaid({
-      orderId: charge.metadata.orderId,
+      orderId: object.metadata.orderId,
       paymentResult: {
-        id: charge.id,
+        id: object.id,
         status: 'COMPLETED',
-        email_address: charge.billing_details.email!,
-        pricePaid: (charge.amount / 100).toFixed(),
+        email_address: object.billing_details.email!,
+        pricePaid: (object.amount / 100).toFixed(),
       },
     });
 
@@ -38,7 +34,6 @@ export async function POST(req: NextRequest) {
       message: 'updateOrderToPaid was successful',
     });
   }
-
   return NextResponse.json({
     message: 'event is not charge.succeeded',
   });
