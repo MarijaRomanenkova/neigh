f70b80188a53fec05da8d18dfa77e1ccf86b4ccf
Marija <marija@dailyart.eu>
2025-03-04 14:36:38 +0200

    stripe up

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -2,17 +2,23 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 import { updateOrderToPaid } from '@/lib/actions/order.actions';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-02-24.acacia'
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest) {
-  // Build the webhook event
-  const event = await stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get('stripe-signature') as string,
-    process.env.STRIPE_WEBHOOK_SECRET!
-  );
+  if(req.method === 'POST'){
+    const signature = req.headers.get('stripe-signature') as string;
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        await req.text(),
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
+    } catch (err) {
+      const error = err as Error;
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
 
   // Check for successful payment
   if (event.type === 'charge.succeeded') {
@@ -37,4 +43,5 @@ export async function POST(req: NextRequest) {
   return NextResponse.json({
     message: 'event is not charge.succeeded',
   });
+}
 }
