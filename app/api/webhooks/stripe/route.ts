c2676187d39ef0fb98f13334df973ea32ed9c65c
Marija <marija@dailyart.eu>
2025-03-04 16:28:51 +0200

    fixing stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -5,33 +5,32 @@ import { updateOrderToPaid } from '@/lib/actions/order.actions';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest) {
-  if(req.method === 'POST'){
-    const signature = req.headers.get('stripe-signature') as string;
-    let event: Stripe.Event;
+  const signature = req.headers.get('stripe-signature') as string;
+  let event: Stripe.Event;
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        await req.text(),
-        signature,
-        process.env.STRIPE_WEBHOOK_SECRET!
-      );
-    } catch (err) {
-      const error = err as Error;
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    }
+  try {
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (err) {
+    const error = err as Error;
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
 
   // Check for successful payment
   if (event.type === 'charge.succeeded') {
-    const { object } = event.data;
+    const charge = event.data.object as Stripe.Charge;
 
     // Update order status
     await updateOrderToPaid({
-      orderId: object.metadata.orderId,
+      orderId: charge.metadata.orderId,
       paymentResult: {
-        id: object.id,
+        id: charge.id,
         status: 'COMPLETED',
-        email_address: object.billing_details.email!,
-        pricePaid: (object.amount / 100).toFixed(),
+        email_address: charge.billing_details.email!,
+        pricePaid: (charge.amount / 100).toFixed(),
       },
     });
 
@@ -43,5 +42,4 @@ export async function POST(req: NextRequest) {
   return NextResponse.json({
     message: 'event is not charge.succeeded',
   });
-}
 }
